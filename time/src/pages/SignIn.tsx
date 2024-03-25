d46387d341266cbf908db662e14337061fc93399
최지원 <wldnjssem6@naver.com>
2024-03-25 15:39:05 +0900

    feat: 로그인 화면
    로그인 화면 구현, 스타일 적용

---
+++ time/src/pages/SignIn.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
+
+function SignIn() {
+    return (
+        <View style={styles.container}>
+            <View style={styles.mainDisplay}>
+                <Image source={require('../assets/images/loginLogo.png')} style={styles.mainLogo}></Image>
+            </View>
+
+            <View style={styles.imageContainer}>
+                <Image source={require('../assets/images/kakao_login.png')} style={styles.image}></Image>
+            </View>
+        </View>
+        
+    );
+}
+
+const styles=StyleSheet.create({
+    container:{
+        height:Dimensions.get('screen').height,
+        backgroundColor:'#C9BAE5',
+        
+    },
+    mainDisplay:{
+        alignItems:'center',
+        justifyContent:'center',
+        padding:100,
+        
+    },
+    mainLogo:{
+        padding:20,
+        marginBottom:20,
+        marginEnd:10,
+    },
+    
+    imageContainer:{
+        flex:1,
+        justifyContent:'center',
+    },
+    image:{
+        width:300,
+        alignSelf:'center',
+        marginBottom:80,
+    }
+})
+
+
+
+export default SignIn;
