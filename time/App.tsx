9c66135ba733f2fcdeb3a0fc9c9945207e2bae10
최지원 <wldnjssem6@naver.com>
2024-03-29 02:08:08 +0900

    feat: bottomTabNavigator로 네비게이션 바
    공통 네비게이션바 만들어서 연결

--- time/App.tsx
+++ time/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import * as React from 'react';
 import SignIn from './src/pages/SignIn'
 import { View } from 'react-native';
+import Main from './src/pages/Main';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import StyleSheet from 'styled-components/dist/sheet/Sheet';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Mypage from './src/pages/Mypage';
+import Nav from './src/navigation/BottmTabNavigation';
+
+// const Stack=createStackNavigator()
+// const Tab=createBottomTabNavigator()
 
 function App() {
   return (
-    <View>
-        <SignIn/>
-    </View>
+    
+      <NavigationContainer>
+      {/* <Stack.Navigator>
+        <Stack.Screen name='Home' component={Main}/>
+      </Stack.Navigator> */}
+        <Nav/>
+      </NavigationContainer>
+    
   );
 }
+
 
 export default App;
---
+++ time/src/navigation/BottmTabNavigation.tsx
@@ -0,0 +1,73 @@
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React from 'react';
+import Main from '../pages/Main';
+import Mypage from '../pages/Mypage';
+import Scrap from '../pages/Scrap';
+import Posting from '../pages/Posting';
+import Chatting from '../pages/Chatting';
+import Octicons from 'react-native-vector-icons/Octicons'
+import Ionicons from 'react-native-vector-icons/Ionicons'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+
+
+const Tab=createBottomTabNavigator()
+const BottmTabNavigation = () => {
+    return (
+        <Tab.Navigator initialRouteName='main'
+        screenOptions={{
+            tabBarStyle:{
+                height: 70,
+            }
+        }}
+        >
+            <Tab.Screen 
+            name='틈새시장' 
+            component={Main}
+            options={{
+                headerTitleStyle:{
+                    color:'#352456',
+                    fontFamily:'Gugi-Regular',
+                    fontSize:35,
+                    paddingTop:20,
+                },
+                tabBarLabel:'홈',
+                tabBarIcon: ()=> <Octicons name='home' size={20}/>
+            }}
+            />
+            <Tab.Screen 
+            name='관심목록' 
+            component={Scrap}
+            options={{
+                tabBarLabel:'관심',
+                tabBarIcon:()=><AntDesign name='hearto' size={20}/>
+            }}
+            />
+            <Tab.Screen 
+            name='글 작성' 
+            component={Posting}
+            options={{
+                tabBarLabel:'',
+                tabBarIcon:()=><AntDesign name='pluscircle' size={35}/>
+            }}
+            />
+            <Tab.Screen 
+            name='채팅' 
+            component={Chatting}
+            options={{
+                tabBarLabel:'채팅',
+                tabBarIcon:()=><Ionicons name='chatbubble-outline' size={20}/>
+            }}
+            />
+            <Tab.Screen 
+            name='나의 틈새' 
+            component={Mypage}
+            options={{
+                tabBarLabel:'마이',
+                tabBarIcon:()=><Ionicons name='person-outline' size={20}/>
+            }}
+            />
+        </Tab.Navigator>
+    );
+};
+
+export default BottmTabNavigation;
