a6519c5ad0425e2f8434682ad43d4947442199c3
ghdtnals <hongsumin58@gmail.com>
2024-04-04 02:38:53 +0900

    feat: BottmTabNavigation 수정

--- time/App.tsx
+++ time/App.tsx
@@ -1,12 +1,10 @@
-
 import * as React from 'react';
-import SignIn from './src/pages/SignIn'
-import { Text, View } from 'react-native';
+import SignIn from './src/pages/SignIn';
+import {Text, View} from 'react-native';
 import Main from './src/pages/Main';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import StyleSheet from 'styled-components/dist/sheet/Sheet';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import Mypage from './src/pages/Mypage';
 import Nav from './src/navigation/BottmTabNavigation';
 // import StackNav from './src/navigation/StackNavigator'
@@ -14,16 +12,12 @@ import myBuyTime from './src/pages/MyBuyTime';
 import Profile from './src/pages/Profile';
 import Notify from './src/pages/Notify';
 
-
-
-
 function App() {
   return (
-      <NavigationContainer>    
-        <Nav/>
-      </NavigationContainer>
+    <NavigationContainer>
+      <Nav />
+    </NavigationContainer>
   );
 }
-
 
 export default App;
