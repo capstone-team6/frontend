5180e77c06987cd11c62e3d71fb17f0f2b1e90d5
최지원 <wldnjssem6@naver.com>
2024-04-18 02:32:21 +0900

    feat: 메인 화면 들어가기 전 stack navigation
    메인 화면 들어가기 전에 나오는 화면들에 대한 stack navigation screen 지정

---
+++ time/src/navigation/StackNavigator.tsx
@@ -0,0 +1,33 @@
+// StackNavigator.tsx
+
+import React from 'react';
+import { StackNavigationProp, createStackNavigator } from '@react-navigation/stack';
+import SignIn from '../pages/SignIn';
+import SignUp from '../pages/SignUp';
+import BottomTabNavigation from './BottmTabNavigation'
+import { RootStackParamList } from '../../types/Type';
+import DeleteMem from '../pages/DeleteMem';
+
+const Stack = createStackNavigator<RootStackParamList>();
+interface StackNavProps{
+    onLoginSuccess:()=>void
+}
+const StackNavigators:React.FC<StackNavProps>=({onLoginSuccess}) => {
+    
+
+    return (
+    <Stack.Navigator>
+        <Stack.Screen name='SignIn' options={{headerShown:false}}>
+            {props => <SignIn {...props} onLoginSuccess={onLoginSuccess} />}
+        </Stack.Screen>
+        <Stack.Screen name='SignUp' component={SignUp}/>
+        <Stack.Screen name='BottomTabNavigation' component={BottomTabNavigation}
+        options={{
+            headerShown:false
+        }}
+        />
+    </Stack.Navigator>
+);
+};
+
+export default StackNavigators;
