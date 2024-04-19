e5221310b6535721bc4c0999b612326b000cef18
ghdtnals <hongsumin58@gmail.com>
2024-04-19 23:13:25 +0900

    rename: ChatStackNavigator

--- time/src/navigation/BottmTabNavigation.tsx
+++ time/src/navigation/BottmTabNavigation.tsx
@@ -11,7 +11,7 @@ import {View} from 'react-native';
 import Feather from 'react-native-vector-icons/Feather';
 import Icon from 'react-native-vector-icons/AntDesign';
 import myPageStack from '../navigation/myPageStackNavigator';
-import ChatStackNavigator from './ChatStackNavigator';
+import ChatStackNavigator from './chatStackNavigator';
 
 const Tab = createBottomTabNavigator();
 
