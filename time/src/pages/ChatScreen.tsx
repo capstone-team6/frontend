759ec957ce8d0c799913c86b43800159c3919a3e
ghdtnals <hongsumin58@gmail.com>
2024-04-27 19:08:27 +0900

    fix: ChatScreen 컴포넌트 수정

--- time/src/pages/ChatScreen.tsx
+++ time/src/pages/ChatScreen.tsx
@@ -18,7 +18,7 @@ import {useRoute, RouteProp, useNavigation} from '@react-navigation/native';
 import {StackNavigationProp} from '@react-navigation/stack';
 import {RootStackParamList} from '../../types/Type';
 import SockJS from 'sockjs-client';
-import Stomp from 'stompjs';
+import Stomp, {Client} from 'stompjs';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 
 type ChatScreenRouteProp = RouteProp<RootStackParamList, 'ChatScreen'>;
@@ -35,30 +35,12 @@ interface Props {
 const ChatScreen: React.FC<Props> = ({route, navigation}) => {
   //Chatting에서 roomId
   const {roomId} = route.params;
-
   const [chatList, setChatList] = useState<
     {roomId: number; writer: string; message: string; type: string}[]
   >([]);
   const [stompClient, setStompClient] = useState<Stomp.Client | null>(null);
   const [messageInput, setMessageInput] = useState('');
   const [modalVisible, setModalVisible] = useState(false);
-
-  const sendMessage = () => {
-    if (stompClient !== null) {
-      const message = {
-        roomId: roomId,
-        writer: 'Me', // 작성자는 사용자로 지정합니다.
-        message: messageInput,
-        type: 'text', // 메시지 유형은 텍스트로 지정합니다. 다른 유형을 사용하는 경우 이 값을 수정하세요.
-      };
-      stompClient.send(
-        `/pub/chat/send/${roomId}`,
-        {},
-        JSON.stringify({message}),
-      );
-      setMessageInput(''); // 메시지를 보낸 후에는 입력 필드를 비웁니다.
-    }
-  };
 
   const goBack = () => {
     navigation.goBack();
@@ -69,53 +51,105 @@ const ChatScreen: React.FC<Props> = ({route, navigation}) => {
     setModalVisible(false);
   };
 
+  const onTransactionPress = () => {
+    if (stompClient !== null) {
+      const transactionMessage = {
+        roomId: roomId,
+        writer: 'Me',
+        message: '거래가 시작됐어요.\n결제 방법을 선택해주세요.',
+        type: 'system_message',
+      };
+      // setChatList(prevMessages => [...prevMessages, transactionMessage]);
+      stompClient.send(
+        `/pub/chat/send`,
+        {},
+        JSON.stringify({transactionMessage}),
+      );
+      addMessage(transactionMessage);
+    }
+    setModalVisible(false);
+  };
+
   const onGalleryPress = () => {
     // 갤러리 접근 코드 작성
     setModalVisible(false);
   };
 
-  const onTransactionPress = () => {
-    // 거래 진행 버튼 코드 작성
-    setModalVisible(false);
+  const onMeet = () => {
+    const meetMessage = {
+      roomId: roomId,
+      writer: 'System',
+      message:
+        '"만나서 결제"로 거래가 진행될 예정이에요.\n해당 글이 거래중 상태로 변경되었어요.',
+      type: 'System',
+    };
+    setChatList(prevMessages => [...prevMessages, meetMessage]);
+    meetComplete();
   };
 
-  useEffect(() => {
-    const initializeWebSocket = () => {
-      const socket = new SockJS('http://13.125.118.92:8080/ws');
-      const client = Stomp.over(socket);
-
-      client.connect({}, () => {
-        console.log('Connected to WebSocket');
-        setStompClient(client);
-
-        client.subscribe(`/sub/chat/room/${roomId}`, (message: any) => {
-          const newMessage = JSON.parse(message.body);
-          setChatList(prevMessages => [...prevMessages, newMessage]);
-        });
-      });
+  const meetComplete = () => {
+    const meetComplete = {
+      roomId: roomId,
+      writer: 'Me',
+      message:
+        '거래를 완료했다면 "거래 완료"를 눌러주세요.\n3일 후에 자동으로 "거래완료"상태로 변경됩니다.',
+      type: 'change',
     };
+    setChatList(prevMessages => [...prevMessages, meetComplete]);
+  };
+
+  const completeMessage = () => {
+    const completeMessage = {
+      roomId: roomId,
+      writer: 'System',
+      message: '거래가 완료되었어요. 상대방의 후기를 남겨주세요.',
+      type: 'System',
+    };
+    setChatList(prevMessages => [...prevMessages, completeMessage]);
+  };
+
+  const onTransfer = () => {};
+  const onPocketPay = () => {};
+
+  useEffect(() => {
+    const socket = new SockJS('http://13.125.118.92:8080/ws');
+    const client = Stomp.over(socket);
+    setStompClient(client);
+    client.connect({}, () => {
+      console.log('Connected to WebSocket');
+      client.subscribe(`/sub/chat/room/${roomId}`, (message: any) => {
+        const newMessage = JSON.parse(message.body);
+        console.log(newMessage);
+        addMessage(newMessage);
+      });
+    });
 
     const disconnectCallback = () => {
       console.log('Disconnected from server');
     };
-
-    initializeWebSocket();
 
     return () => {
       if (stompClient !== null) {
         stompClient.disconnect(disconnectCallback);
       }
     };
-  }, [chatList]);
+  }, [chatList, roomId]);
 
-  const [showTopInfo, setShowTopInfo] = useState(true);
+  const addMessage = (message: any) => {
+    setChatList(prev => [...prev, message]);
+  };
 
-  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
-    const scrollY = event.nativeEvent.contentOffset.y;
-    if (scrollY > 100) {
-      setShowTopInfo(false);
-    } else {
-      setShowTopInfo(true);
+  const sendMessage = () => {
+    if (stompClient !== null) {
+      const message = {
+        roomId: roomId,
+        writer: 'Me',
+        message: messageInput,
+        type: 'text',
+      };
+      stompClient.send(`/pub/chat/send/`, {}, JSON.stringify({message}));
+      setChatList(prevMessages => [...prevMessages, message]);
+      setMessageInput('');
     }
   };
 
@@ -131,7 +165,7 @@ const ChatScreen: React.FC<Props> = ({route, navigation}) => {
           <AntDesign name="arrowleft" size={24} color="black" />
         </TouchableOpacity>
       </View>
-      {showTopInfo && (
+      <ScrollView>
         <View
           style={{
             width: Dimensions.get('screen').width,
@@ -162,23 +196,178 @@ const ChatScreen: React.FC<Props> = ({route, navigation}) => {
             지금까지 5번의 시간을 거래했어요.
           </Text>
         </View>
-      )}
-      <ScrollView style={{flex: 1}} onScroll={onScroll}>
         {chatList.map((msg, index) => (
           <View
             key={index}
             style={{
-              alignSelf: msg.writer === 'Me' ? 'flex-end' : 'flex-start',
-              margin: 5,
+              alignSelf:
+                msg.writer === 'Me'
+                  ? 'flex-end'
+                  : msg.writer === 'System'
+                  ? 'center'
+                  : 'flex-start',
+              margin: 10,
+
+              width: msg.writer === 'System' ? '80%' : 'auto',
             }}>
-            <Text
-              style={{
-                backgroundColor: msg.writer === 'Me' ? '#C9BAE5' : '#F1F1F1',
-                padding: 10,
-                borderRadius: 8,
-              }}>
-              {msg.message}
-            </Text>
+            {msg.type === 'system_message' ? (
+              <View
+                style={{
+                  backgroundColor: '#F1F1F1',
+                  padding: 13,
+                  borderRadius: 8,
+                }}>
+                <Text
+                  style={{
+                    fontFamily: 'NanumGothic',
+                    fontSize: 17,
+                    color: 'black',
+                  }}>
+                  {msg.message}
+                </Text>
+
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#C9BAE5',
+                      width: '100%',
+                      height: 35,
+                      borderRadius: 10,
+                      justifyContent: 'center',
+                      marginVertical: 8,
+                    }}
+                    onPress={onMeet}>
+                    <Text
+                      style={{
+                        fontFamily: 'NanumGothic',
+                        fontSize: 15,
+                        textAlign: 'center',
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      만나서 결제
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#C9BAE5',
+                      width: '100%',
+                      height: 35,
+                      borderRadius: 10,
+                      justifyContent: 'center',
+                      marginBottom: 8,
+                    }}
+                    onPress={onTransfer}>
+                    <Text
+                      style={{
+                        fontFamily: 'NanumGothic',
+                        textAlign: 'center',
+                        color: 'black',
+                        fontWeight: 'bold',
+                        fontSize: 15,
+                      }}>
+                      계좌이체
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#C9BAE5',
+                      width: '100%',
+                      height: 35,
+                      borderRadius: 10,
+                      justifyContent: 'center',
+                    }}
+                    onPress={onPocketPay}>
+                    <Text
+                      style={{
+                        fontFamily: 'NanumGothic',
+                        textAlign: 'center',
+                        color: 'black',
+                        fontWeight: 'bold',
+                        fontSize: 15,
+                      }}>
+                      틈새페이
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : msg.type == 'change' ? (
+              <View
+                style={{
+                  backgroundColor: '#F1F1F1',
+                  padding: 13,
+                  borderRadius: 8,
+                }}>
+                <Text
+                  style={{
+                    fontFamily: 'NanumGothic',
+                    fontSize: 17,
+                    color: 'black',
+                  }}>
+                  {msg.message}
+                </Text>
+                <View>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#C9BAE5',
+                      width: '100%',
+                      height: 35,
+                      borderRadius: 10,
+                      justifyContent: 'center',
+                      marginVertical: 8,
+                    }}
+                    onPress={completeMessage}>
+                    <Text
+                      style={{
+                        fontFamily: 'NanumGothic',
+                        fontSize: 15,
+                        textAlign: 'center',
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      거래 완료
+                    </Text>
+                  </TouchableOpacity>
+                  <TouchableOpacity
+                    style={{
+                      backgroundColor: '#C9BAE5',
+                      width: '100%',
+                      height: 35,
+                      borderRadius: 10,
+                      justifyContent: 'center',
+                      marginVertical: 8,
+                    }}
+                    onPress={completeMessage}>
+                    <Text
+                      style={{
+                        fontFamily: 'NanumGothic',
+                        fontSize: 15,
+                        textAlign: 'center',
+                        color: 'black',
+                        fontWeight: 'bold',
+                      }}>
+                      거래 취소
+                    </Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            ) : (
+              <Text
+                style={{
+                  color: 'black',
+                  backgroundColor:
+                    msg.writer === 'Me'
+                      ? '#F1F1F1'
+                      : msg.writer === 'System'
+                      ? '#BDBBC2'
+                      : '#C9BAE5',
+                  padding: 10,
+                  borderRadius: 8,
+                  textAlign: msg.writer === 'System' ? 'center' : 'auto',
+                }}>
+                {msg.message}
+              </Text>
+            )}
           </View>
         ))}
       </ScrollView>
@@ -197,7 +386,7 @@ const ChatScreen: React.FC<Props> = ({route, navigation}) => {
         <TextInput
           value={messageInput}
           onChangeText={setMessageInput}
-          onSubmitEditing={sendMessage} // TextInput에서 엔터를 누를 때 sendMessage 함수 호출
+          onSubmitEditing={sendMessage}
           placeholder="Type your message..."
           style={{
             flex: 1,
